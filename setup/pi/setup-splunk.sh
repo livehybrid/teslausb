562f39548bbdd839f1ee66f966e0cc98ccdb8e58
livehybrid <5527349+livehybrid@users.noreply.github.com>
2021-06-05 23:02:49 +0100

    Update setup-splunk.sh

--- setup/pi/setup-splunk.sh
+++ setup/pi/setup-splunk.sh
@@ -4,7 +4,7 @@ cd /opt
 wget -O splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=ARMv6&platform=linux&version=8.1.3&product=universalforwarder&filename=splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz&wget=true'
 tar -xf splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz
 rm -f splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz
-adduser splunk
+adduser --gecos "" --disabled-password splunk
 cat <<EOF | sudo -u splunk tee /opt/splunkforwarder/etc/system/local/user-seed.conf
 username=admin
 password=Password1
