60f05d4667452accac4bf5035ca62e879b7b999e
livehybrid <5527349+livehybrid@users.noreply.github.com>
2021-06-06 23:37:32 +0100

    Update setup-splunk.sh

--- setup/pi/setup-splunk.sh
+++ setup/pi/setup-splunk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
-
-cd /opt
+cd /mutable
+ln -s mk
 wget -O splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=ARMv6&platform=linux&version=8.1.3&product=universalforwarder&filename=splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz&wget=true'
 tar -xf splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz
 rm -f splunkforwarder-8.1.3-63079c59e632-Linux-arm.tgz
@@ -10,6 +10,9 @@ username=admin
 password=Password1
 EOF
 
-chown -hR splunk /opt/splunkforwarder
+
+chown -hR splunk /mutable/splunkforwarder
+ln -s /mutable/splunkforwarder /opt/splunkforwarder
 /opt/splunkforwarder/bin/splunk enable boot-start -systemd-managed 0 -user splunk --accept-license
+/opt/splunkforwarder/bin/splunk set deploy-poll 192.168.0.14:8089
 /opt/splunk/bin/splunk start
